aa94e84b7c2783c132c969e59453b623090ff8dd
rpandhare <ranjeet.pandhare@exelaonline.com>
2021-05-18 18:49:00 +0530

    comment user interface

--- userAssignmentFrontEnd/src/app/data-table/data-table.component.ts
+++ userAssignmentFrontEnd/src/app/data-table/data-table.component.ts
@@ -1,28 +1,21 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
-import { Data, Router } from '@angular/router';
-import { error } from 'selenium-webdriver';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CrudService } from '../services/crud.service';
-import { ActivatedRoute } from '@angular/router';
-import { Users } from 'src/app/services/Users'
-import { Pipe, PipeTransform } from '@angular/core';
+import { Users } from 'src/app/services/Users';
 
-
-export interface UserData {
-  id: string,
-  username: string;
-  firstname: string;
-  lastname: string;
-  email: string;
-  phone: string;
-  dob: string;
-  userid: string;
-  gender: string;
-  password: string;
-  varifypassword: string;
-}
+// export interface UserData {
+//   id: string;
+//   username: string;
+//   firstname: string;
+//   lastname: string;
+//   email: string;
+//   phone: string;
+//   dob: string;
+//   userid: string;
+//   gender: string;
+//   password: string;
+//   varifypassword: string;
+// }
 
 /** Constants used to fill up our data base. */
 
@@ -36,73 +29,88 @@ export interface UserData {
 })
 export class DataTableComponent implements OnInit {
   newuser: Users[] = [];
-  showMeTable1:boolean=true;
-  showMeTable2:boolean=true;
-  showMeTable3:boolean=true;
-  showMeTable4:boolean=true;
-  showMeTable5:boolean=true;
-  showMeTable6:boolean=true;
-  showMeTable7:boolean=true;
-  showMeTable8:boolean=true;
-  showMeTable9:boolean=true;
-  hidefilterAction:boolean=false;
+  showMeTable1: boolean = true;
+  showMeTable2: boolean = true;
+  showMeTable3: boolean = true;
+  showMeTable4: boolean = true;
+  showMeTable5: boolean = true;
+  showMeTable6: boolean = true;
+  showMeTable7: boolean = true;
+  showMeTable8: boolean = true;
+  showMeTable9: boolean = true;
+  hidefilterAction: boolean = false;
 
-  show=true;
+  show = true;
 
-  hideShowColumn1(){
-    this.showMeTable1=!this.showMeTable1 
+  hideShowColumn1() {
+    this.showMeTable1 = !this.showMeTable1;
   }
-  hideShowColumn2(){
-    this.showMeTable2=!this.showMeTable2 
+  hideShowColumn2() {
+    this.showMeTable2 = !this.showMeTable2;
   }
-  hideShowColumn3(){
-    this.showMeTable3=!this.showMeTable3 
+  hideShowColumn3() {
+    this.showMeTable3 = !this.showMeTable3;
   }
-  hideShowColumn4(){
-    this.showMeTable4=!this.showMeTable4 
+  hideShowColumn4() {
+    this.showMeTable4 = !this.showMeTable4;
   }
-  hideShowColumn5(){
-    this.showMeTable5=!this.showMeTable5 
+  hideShowColumn5() {
+    this.showMeTable5 = !this.showMeTable5;
   }
-  hideShowColumn6(){
-    this.showMeTable6=!this.showMeTable6 
+  hideShowColumn6() {
+    this.showMeTable6 = !this.showMeTable6;
   }
-  hideShowColumn7(){
-    this.showMeTable7=!this.showMeTable7 
+  hideShowColumn7() {
+    this.showMeTable7 = !this.showMeTable7;
   }
-  hideShowColumn8(){
-    this.showMeTable8=!this.showMeTable8 
+  hideShowColumn8() {
+    this.showMeTable8 = !this.showMeTable8;
   }
-  hideShowColumn9(){
-    this.showMeTable9=!this.showMeTable9 
+  hideShowColumn9() {
+    this.showMeTable9 = !this.showMeTable9;
   }
 
-
-  Users = {
-    _id: '',
-    username: '',
-    firstname: '',
-    lastname: '',
-    email: '',
-    phone: '',
-    dob: "",
-    userid: '',
-    gender: '',
-    passsword: ''
-  }
+  // Users = {
+  //   _id: '',
+  //   username: '',
+  //   firstname: '',
+  //   lastname: '',
+  //   email: '',
+  //   phone: '',
+  //   dob: '',
+  //   userid: '',
+  //   gender: '',
+  //   passsword: '',
+  // };
 
   alert: boolean = false;
 
-  index = ["User Name", "First Name", "Last Name", "Email", "Phone", "DOB", "UserId", "Gender", "Password"];
+  index = [
+    'User Name',
+    'First Name',
+    'Last Name',
+    'Email',
+    'Phone',
+    'DOB',
+    'UserId',
+    'Gender',
+    'Password',
+  ];
 
   displayedColumns = [
-    'username', 'firstname', 'lastname', 'email', 'number', 'dob', 'userid',
-    'gender', 'password'
+    'username',
+    'firstname',
+    'lastname',
+    'email',
+    'number',
+    'dob',
+    'userid',
+    'gender',
+    'password',
   ];
 
   data: any = [];
   p: number = 1;
-
 
   // search filter field
   username: string;
@@ -113,19 +121,14 @@ export class DataTableComponent implements OnInit {
   gender: string;
   password: string;
 
-  constructor(
-    private http: CrudService,
-    private router: Router,
-
-  ) {
-    this.username = "";
-    this.firstname = "";
-    this.lastname = "";
-    this.email = "";
-    this.userid = "";
-    this.gender = "";
-    this.password = ""
-
+  constructor(private http: CrudService, private router: Router) {
+    this.username = '';
+    this.firstname = '';
+    this.lastname = '';
+    this.email = '';
+    this.userid = '';
+    this.gender = '';
+    this.password = '';
 
     this.data = new Array<any>();
   }
@@ -133,109 +136,128 @@ export class DataTableComponent implements OnInit {
   ngOnInit() {
     this.newuser = [];
     this.getTableData();
-
   }
 
-  hashPassword(password: string){
-    this.password=password;
-    return "*".repeat(this.password.length)
+  hashPassword(password: string) {
+    this.password = password;
+    return '*'.repeat(this.password.length);
   }
-
+//search function
   Search() {
-    if (this.username !== "") {
-      this.newuser = this.newuser.filter(res => {
-        return res.username.toLocaleLowerCase().match(this.username.toLocaleLowerCase())
-      })
-    } else if (this.username == "") {
+    if (this.username !== '') {
+      this.newuser = this.newuser.filter((res) => {
+        return res.username
+          .toLocaleLowerCase()
+          .match(this.username.toLocaleLowerCase());
+      });
+    } else if (this.username == '') {
       this.ngOnInit();
     }
   }
+  //username filter
   UsernameFilter() {
-    if (this.username !== "") {
-      this.newuser = this.newuser.filter(res => {
-        return res.username.toLocaleLowerCase().match(this.username.toLocaleLowerCase())
-      })
-    } else if (this.username == "") {
+    if (this.username !== '') {
+      this.newuser = this.newuser.filter((res) => {
+        return res.username
+          .toLocaleLowerCase()
+          .match(this.username.toLocaleLowerCase());
+      });
+    } else if (this.username == '') {
       this.ngOnInit();
     }
   }
+  //firstname filter
   firstnameFilter() {
-    if (this.firstname !== "") {
-      this.newuser = this.newuser.filter(res => {
-        return res.firstname.toLocaleLowerCase().match(this.firstname.toLocaleLowerCase())
-      })
-    } else if (this.firstname == "") {
+    if (this.firstname !== '') {
+      this.newuser = this.newuser.filter((res) => {
+        return res.firstname
+          .toLocaleLowerCase()
+          .match(this.firstname.toLocaleLowerCase());
+      });
+    } else if (this.firstname == '') {
       this.ngOnInit();
     }
   }
+  //lastname filter
   lastnameFilter() {
-    if (this.lastname !== "") {
-      this.newuser = this.newuser.filter(res => {
-        return res.lastname.toLocaleLowerCase().match(this.lastname.toLocaleLowerCase())
-      })
-    } else if (this.lastname == "") {
+    if (this.lastname !== '') {
+      this.newuser = this.newuser.filter((res) => {
+        return res.lastname
+          .toLocaleLowerCase()
+          .match(this.lastname.toLocaleLowerCase());
+      });
+    } else if (this.lastname == '') {
       this.ngOnInit();
     }
   }
+  //email filter
   emailFilter() {
-    if (this.email !== "") {
-      this.newuser = this.newuser.filter(res => {
-        return res.email.toLocaleLowerCase().match(this.email.toLocaleLowerCase())
-      })
-    } else if (this.email == "") {
+    if (this.email !== '') {
+      this.newuser = this.newuser.filter((res) => {
+        return res.email
+          .toLocaleLowerCase()
+          .match(this.email.toLocaleLowerCase());
+      });
+    } else if (this.email == '') {
       this.ngOnInit();
     }
   }
+  //userid filter
   useridFilter() {
-    if (this.userid !== "") {
-      this.newuser = this.newuser.filter(res => {
-        return res.userid.toLocaleLowerCase().match(this.userid.toLocaleLowerCase())
-      })
-    } else if (this.userid == "") {
+    if (this.userid !== '') {
+      this.newuser = this.newuser.filter((res) => {
+        return res.userid
+          .toLocaleLowerCase()
+          .match(this.userid.toLocaleLowerCase());
+      });
+    } else if (this.userid == '') {
       this.ngOnInit();
     }
   }
+  //gender filter
   genderFilter() {
-    if (this.gender !== "") {
-      this.newuser = this.newuser.filter(res => {
-        return res.gender.toLocaleLowerCase().match(this.gender.toLocaleLowerCase())
-      })
-    } else if (this.gender == "") {
+    if (this.gender !== '') {
+      this.newuser = this.newuser.filter((res) => {
+        return res.gender
+          .toLocaleLowerCase()
+          .match(this.gender.toLocaleLowerCase());
+      });
+    } else if (this.gender == '') {
       this.ngOnInit();
     }
   }
 
   key: string = 'username';
   reverse: boolean = false;
+  //sorting 
   sort(key: string) {
     console.log(key);
     this.key = key;
     this.reverse = !this.reverse;
   }
-
+//get data from db
   getTableData() {
     this.http.getData().subscribe((d) => {
       // for(let entry of d){
-      //   console.log(entry.password); 
+      //   console.log(entry.password);
       // }
       this.data = d;
       this.newuser = d;
       console.log(d.password);
       console.log(d);
-    })
+    });
   }
 
   //delete user
   deleteUser(id: string) {
-    // alert("are you sure want to delete ...?")
     this.http.deleteUser(id).subscribe(() => {
-      console.log("data deleted in database");
-      this.router.navigateByUrl('/dashboard')
+      console.log('data deleted in database');
+      this.router.navigateByUrl('/dashboard');
       //  this.reloadData();
-    })
+    });
   }
 }
 
-function d(d: any) {
-  throw new Error('Function not implemented.');
-}
+// function d(d: any) {
+//   throw new Error('Function not implemented.');
+// }
